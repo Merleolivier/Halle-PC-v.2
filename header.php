e05b34af6b81a5e6cb6b3a0fca8efc0f8f581fb4
Olivier Merle <oliver51460@gmail.com>
2018-05-03 00:23:12 +0200

    UP

---
+++ header.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>H@LLE PC</title>
+  <style type="text/css">
+  ul {
+    list-style-type: none;
+}
+  </style>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.11/css/all.css" integrity="sha384-p2jx59pefphTFIpeqCcISO9MdVfIm4pNnsL08A6v5vaQc4owkQqxMV8kg4Yvhaw/" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Abel" rel="stylesheet"> 
+    <meta charset="UTF-8"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+    <meta name="author" content="Olivier Merle" />
+    <meta name="keywords" content="halle pc, reparation pc, reparation telephone" />
+</head>
+<body>
+
+   <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#"><img src="assets/img/logo.png" width="100"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="H@lle pc">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+	      <a class="nav-link" href="index.html"><font face="Abel"><i class="fa fa-home"></i> Acceuil</font><span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="ordinateur.html"><font face="Abel"><i class="fa fa-laptop"></i> Ordinateur</font></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="périphérique.html"><font face="Abel"><i class="fa fa-print"></i> Périphérique</font></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="composant.html"><font face="Abel"><i class="fa fa-cogs"></i> Composant</font></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="consommable.html"><font face="Abel"><i class="fa fa-plus-square"></i> Consommable</font></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="téléphonie.html"><font face="Abel"><i class="fa fa-phone"></i> Téléphonie</font></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="occasion.html"><font face="Abel"><i class="fa fa-archive"></i> Occasion</font></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="suivez-nous.html"><font face="Abel"><i class="fa fa-share-alt"></i> Suivez-nous</font></a>
+      </li>
+    </ul>
+  </div>
+</nav>
--- index.php
+++ index.php
@@ -1,56 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>H@LLE PC</title>
-  <style type="text/css">
-  ul {
-    list-style-type: none;
-}
-  </style>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.11/css/all.css" integrity="sha384-p2jx59pefphTFIpeqCcISO9MdVfIm4pNnsL08A6v5vaQc4owkQqxMV8kg4Yvhaw/" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=Abel" rel="stylesheet"> 
-    <meta charset="UTF-8"/>
-    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
-    <meta name="author" content="Olivier Merle" />
-    <meta name="keywords" content="halle pc, reparation pc, reparation telephone" />
-</head>
-<body>
-
-   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#"><img src="assets/img/logo.png" width="100"></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="H@lle pc">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-	      <a class="nav-link" href="index.html"><font face="Abel"><i class="fa fa-home"></i> Acceuil</font><span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="ordinateur.html"><font face="Abel"><i class="fa fa-laptop"></i> Ordinateur</font></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="périphérique.html"><font face="Abel"><i class="fa fa-print"></i> Périphérique</font></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="composant.html"><font face="Abel"><i class="fa fa-cogs"></i> Composant</font></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="consommable.html"><font face="Abel"><i class="fa fa-plus-square"></i> Consommable</font></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="téléphonie.html"><font face="Abel"><i class="fa fa-phone"></i> Téléphonie</font></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="occasion.html"><font face="Abel"><i class="fa fa-archive"></i> Occasion</font></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="suivez-nous.html"><font face="Abel"><i class="fa fa-share-alt"></i> Suivez-nous</font></a>
-      </li>
-    </ul>
-  </div>
-</nav>
+<?php
+require_once('header.php');
+?>
 <FONT color="black"> 
 <div style="background-color:grey;" class="jumbotron jumbotron-fluid">
   <div class="container">
@@ -87,6 +37,9 @@ Vous trouverez dans notre établissement tout le nécessaire : </p>
     <p>Nous sommes en partenariat avec une radio web qui ai <a href="hitsmixradio.com">Hitsmixradio.</a>
   </div>
 </div>
+<?php
+require_once('footer.php');
+?>
 
 
 
@@ -94,5 +47,5 @@ Vous trouverez dans notre établissement tout le nécessaire : </p>
 
 
 
-</body>
-</html>
+
+
